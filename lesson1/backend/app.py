ca95811f0c31793e077a25f6f10a07cbc5e1a03d
folderplus <ww820910@gmail.com>
2022-09-23 15:40:46 +0800

    create mongo db

--- lesson1/backend/app.py
+++ lesson1/backend/app.py
@@ -1,9 +1,36 @@
+from typing import Optional
+
+from beanie import init_beanie, Document
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
 
-app = FastAPI()
 
+class Greeting(Document):
+    message: str
+
+
+async def init():
+    # Create Motor client
+    client = AsyncIOMotorClient(
+        "mongodb://admin-user:admin-password@192.168.6.63:27017"
+    )
+
+    # Initialize beanie with the Product document class and a database
+    await init_beanie(database=client.admin, document_models=[Greeting])
+
+app = FastAPI(on_startup=[init])
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.post("/")
+async def create(name: str = "world"):
+    message = Greeting(message=f"Hello {name}!")
+    await message.save()
+    return message
+
+@app.get("/all")
+async def get_all():
+    greetings = await Greeting.find().to_list()
+    return greetings
